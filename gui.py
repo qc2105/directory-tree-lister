7d2ffba5c85859340b50345975d39f3db6f88b3a
FriedPigeon <FriedPigeon@users.noreply.github.com>
2017-01-19 18:24:21 +1100

    Small refactor for clarity, and added some comments.

--- gui.py
+++ gui.py
@@ -21,40 +21,43 @@ class Application(tk.Frame):
         Creates Buttons and Labels.
         """
         # Choose a directory button
-        self.ask_dir = tk.Button(self, text='Choose a directory', command=self.ask_directory)
-        self.ask_dir.pack()
+        self.ask_dir_btn = tk.Button(self, text='Choose a directory', command=self.ask_directory)
+        self.ask_dir_btn.pack()
 
         # Text button
-        self.make_list_text = tk.Button(self, text="Create Text File Directory Tree", state='disabled')
-        self.make_list_text.pack()
+        self.make_list_text_btn = tk.Button(self, text="Create Text File Directory Tree", state='disabled')
+        self.make_list_text_btn.pack()
 
         # Excel button
-        self.make_list_excel = tk.Button(self, text="Create Excel File Directory Tree", state='disabled')
-        self.make_list_excel.pack()
+        self.make_list_excel_btn = tk.Button(self, text="Create Excel File Directory Tree", state='disabled')
+        self.make_list_excel_btn.pack()
 
         # Quit button
-        self.quit = tk.Button(self, text="Quit", command=root.destroy)
-        self.quit.pack()
+        self.quit_btn = tk.Button(self, text="Quit", command=root.destroy)
+        self.quit_btn.pack()
 
         # Selected directory Label
-        self.selected_dir = tk.Label(self.master, text='', wraplength=375)
-        self.selected_dir.pack()
+        self.selected_dir_label = tk.Label(self.master, text='', justify='left', wraplength=375)
+        self.selected_dir_label.pack()
 
         # Output Label
-        self.output = tk.Label(self.master, text='', wraplength=375)
-        self.output.pack()
+        self.output_label = tk.Label(self.master, text='', justify='left', wraplength=375)
+        self.output_label.pack()
 
     def ask_directory(self) -> None:
         """
         Creates a file dialog and enables and loads methods into Make List buttons.
         """
+        # Ask directory
         self.dirname = tk.filedialog.askdirectory(initialdir='.')
 
         if self.dirname:
-            self.selected_dir.config(text='Directory:\n{}'.format(self.dirname), justify='left')
+            # Outputs message of selected directory to GUI
+            self.selected_dir_label.config(text='Directory:\n{}'.format(self.dirname))
 
-            self.make_list_text.config(state='active', command=partial(self.make_dir_tree_excel, self.dirname))
-            self.make_list_excel.config(state='active', command=partial(self.make_dir_tree_text, self.dirname))
+            # Make buttons active
+            self.make_list_text_btn.config(state='active', command=partial(self.make_dir_tree_excel, self.dirname))
+            self.make_list_excel_btn.config(state='active', command=partial(self.make_dir_tree_text, self.dirname))
 
     def make_dir_tree_excel(self, directory: str) -> None:
         """
@@ -66,11 +69,14 @@ class Application(tk.Frame):
         directory_tree_lister.list_directory_tree_excel(directory)
 
         file_name = directory.split('/')[-1]
-        output_file = '{path}/{file}'.format(
-            path=os.path.dirname(os.path.abspath(__file__)), file='directory-tree-{}.xlsx'.format(file_name)
+
+        output_file_location = '{path}/{file}'.format(
+            path=os.path.dirname(os.path.abspath(__file__)),
+            file='directory-tree-{}.xlsx'.format(file_name)
         )
 
-        self.output.config(text='Created excel file:\n{}'.format(output_file), justify='left')
+        # Outputs message of file created to GUI
+        self.output_label.config(text='Created excel file:\n{}'.format(output_file_location))
 
     def make_dir_tree_text(self, directory: str) -> None:
         """
@@ -82,11 +88,14 @@ class Application(tk.Frame):
         directory_tree_lister.list_directory_tree_text(directory)
 
         file_name = directory.split('/')[-1]
-        output_file = '{path}/{file}'.format(
-            path=os.path.dirname(os.path.abspath(__file__)), file='directory-tree-{}.txt'.format(file_name)
+
+        output_file_location = '{path}/{file}'.format(
+            path=os.path.dirname(os.path.abspath(__file__)),
+            file='directory-tree-{}.txt'.format(file_name)
         )
 
-        self.output.config(text='Created text file:\n{}'.format(output_file), justify='left')
+        # Outputs message of file created to GUI
+        self.output_label.config(text='Created text file:\n{}'.format(output_file_location))
 
 
 if __name__ == '__main__':
